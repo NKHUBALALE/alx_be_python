a5f658182037e6507d0726f475af478724d452bc
Nkhubalale <physimanuel@gmail.com>
2024-06-29 19:22:17 +0200

    editted statements to single lines

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,9 @@
-current_wheather = input("What's the weather like today? (sunny/rainy/cold): ")
+current_weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if current_wheather == "sunny":
-    print("Wear a t-shirt and sunglasses.")
-elif current_wheather == "rainy" :
+if current_weather == "sunny": print("Wear a t-shirt and sunglasses.")
+elif current_weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif current_wheather == "cold" :
+elif current_weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
