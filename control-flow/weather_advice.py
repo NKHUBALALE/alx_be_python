674da7570c7daddf4976579db909a20be028ab80
Nkhubalale <physimanuel@gmail.com>
2024-06-29 19:12:19 +0200

    wheater advice done

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+current_wheather = input("What's the weather like today? (sunny/rainy/cold): ")
+
+if current_wheather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif current_wheather == "rainy" :
+    print("Don't forget your umbrella and a raincoat.")
+elif current_wheather == "cold" :
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
