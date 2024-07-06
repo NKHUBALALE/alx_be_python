ade4bb4701cf38d516b863f32bb6f689a936d4ce
Nkhubalale <physimanuel@gmail.com>
2024-07-06 20:44:11 +0200

    check for zero

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,10 +7,10 @@ def perform_operation(num1, num2, operation):
         elif operation == 'multiply':
             return num1 * num2
         elif operation == 'divide':
-            if num2 != 0:
-                return num1 / num2
+            if num2 == 0 or num1 ==0 :
+                return "Error: Division by zero" 
             else:
-                return "Error: Division by zero"
+                return num1 / num2
         else:
             return "Error: Invalid operation"
     except Exception as e:
